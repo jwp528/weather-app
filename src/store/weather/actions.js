8e0f6cbdca5dc7ee468faeb9e9b67c9062a5173f
Josh <joshwp528@gmail.com>
2019-09-09 17:06:22 -0400

    added updater for current weather

--- src/store/weather/actions.js
+++ src/store/weather/actions.js
@@ -3,10 +3,14 @@ export default {
     fetch: async ({commit}) => {
         try {
             let response = await axios.get('http://api.openweathermap.org/data/2.5/weather?q=London,ca&appid=ecda124003b79d7d5c9fe97c3a6d731d&units=metric');
-
             commit('set', response.data);
+
+            setTimeout(async () => {
+              response = await axios.get('http://api.openweathermap.org/data/2.5/weather?q=London,ca&appid=ecda124003b79d7d5c9fe97c3a6d731d&units=metric');
+              commit('set', response.data);
+            }, 900000);// Every 15 Minutes
         }catch(e) {
             console.error(e);
-        }                
+        }
     }
 }
