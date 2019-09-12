2c7064df0fe43aeb301f5aee7d106fc988cff6d8
Josh <Parsons>
2019-09-12 00:19:18 -0400

    added debug output for light sensor code

--- sensor.py
+++ sensor.py
@@ -8,11 +8,15 @@ GPIO.setmode(GPIO.BOARD)
 light_sensor = 11  # this is the GPIO number on the board we've plugged this into
 
 GPIO.setup(light_sensor, GPIO.IN)
-while true:
+while True:
     i = GPIO.input(light_sensor)
-    if i == 0
-    print("No light detected, shutting down")
-    time.sleep(1)
-    elif i == 1
-    print('Light is on, powering up!')
-    time.sleep(30)
+    if i == 0:
+        print("No light detected, shutting down")
+        #run('vcgencmd display_power 0', shell=True)
+        time.sleep(1)
+    elif i == 1:
+        print('Light is on, powering up!')
+        #run('vcgencmd display_power 1', shell=True)
+        time.sleep(30)
+    else:
+        print('Light sensor detecting greater than 1: ', i)
