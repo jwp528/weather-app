32a29ad9c03b3cabd47ef968b9fcd0b758bcc01b
Josh <Parsons>
2019-09-15 13:45:18 -0400

    updated timing on LED

--- python/LED.py
+++ python/LED.py
@@ -3,15 +3,15 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
 
 try:
-	while True:
-		GPIO.output(18,GPIO.HIGH)
-		print("LED ON")
-		time.sleep(1)
-		GPIO.output(18,GPIO.LOW)
-		print("LED OFF")
-		time.sleep(1)
+    while True:
+        GPIO.output(18, GPIO.HIGH)
+        print("LED ON")
+        time.sleep(.1)
+        GPIO.output(18, GPIO.LOW)
+        print("LED OFF")
+        time.sleep(1)
 except KeyboardInterrupt:
-	GPIO.cleanup()
+    GPIO.cleanup()
