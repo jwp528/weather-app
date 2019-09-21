5c2edfc1f1ad14bc8541df23a595cc83cbe14f11
Josh <Parsons>
2019-09-21 14:35:30 -0400

    added new centering method for news component

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,24 +1,25 @@
 <template>
-  <v-layout align-center justify-center column fill-height class="text-xs-center mt-5">
-    <v-flex xs12>
-      <v-img
-        :src="headline.enclosure.url"
-        width="500"
-        ratio="16:9"
-        class="mt-5 pt-5"
-        style="margin:0 auto"
-      />
-      <h1
-        class="display-3 font-weight-thin text-xs-center"
-        transition="fade-transition"
-      >{{headline.title}}</h1>
-      <h4
-        class="display-1 font-weight-thin text-xs-center"
-        transition="fade-transition"
-      >{{headline.content}}</h4>
-    </v-flex>
+  <v-layout column>
+    <v-layout align-center justify-center row fill-height class="text-xs-center">
+      <v-flex xs12>
+        <v-img :src="headline.enclosure.url" width="500" ratio="16:9" style="margin:0 auto" />
+        <h1
+          class="display-3 font-weight-thin text-xs-center"
+          transition="fade-transition"
+        >{{headline.title}}</h1>
+        <h4
+          class="display-1 font-weight-thin text-xs-center"
+          transition="fade-transition"
+        >{{headline.content}}</h4>
+      </v-flex>
+    </v-layout>
   </v-layout>
 </template>
+<style>
+.imageClass {
+  margin-top: 50px;
+}
+</style>
 <script>
 import { Vue, Component, Prop } from "vue-property-decorator";
 import { State, Getter, Action, Mutation, namespace } from "vuex-class";
