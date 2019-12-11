50180ce82ccb5218f78a8661f48ea78d1a50552c
Josh <Parsons>
2019-12-11 07:16:56 -0500

    undid anniversary changes

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,8 +1,7 @@
 <template>
   <v-layout column>
     <v-layout align-center justify-center row fill-height class="text-xs-center mt-5">
-      <!-- <v-flex xs12 v-if="typeof headline !== 'undefined'"> -->
-      <v-flex xs12 v-if="false">
+      <v-flex xs12 v-if="typeof headline !== 'undefined'">
         <v-img :src="headline.enclosure.url" width="500" ratio="16:9" style="margin:0 auto" />
         <h1
           class="display-3 font-weight-thin text-xs-center"
@@ -13,7 +12,7 @@
           transition="fade-transition"
         >{{headline.content}}</h4>
       </v-flex>
-      <v-flex v-if="false" xs12>
+      <v-flex v-else xs12>
         <v-img
           src="https://via.placeholder.it/600x400?text=Image+Here"
           width="500"
@@ -28,19 +27,6 @@
           class="display-1 font-weight-thin text-xs-center"
           transition="fade-transition"
         >Dumbass london fuck boi can't figure out why this shit isn't working</h4>
-      </v-flex>
-    </v-layout>
-    <v-layout v-if="true" align-center justify-center row fill-height class="text-xs-center mt-5">
-      <v-flex xs12>
-        <v-img :src="currentPhoto.src" :height="currentPhoto.orientation === 'portrait' ? 350 : 'auto'" max-height="350" width="600" ratio="16:9" style="margin:0 auto" />
-        <h1
-          class="display-3 font-weight-thin text-xs-center"
-          transition="fade-transition"
-        >Happy 3rd Anniversary</h1>
-        <h4 class="display-1 font-weight-thin text-xs-center" transition="fade-transition">
-          It's been a few years now and growing with you on this amazing life adventure has been one of the greatest experiences i've ever had. You make me want to be a better man every single day. I love you so much Jessica Sherren.
-          <v-icon>mdi-heart</v-icon>
-        </h4>
       </v-flex>
     </v-layout>
   </v-layout>
--- src/store/news/getters.js
+++ src/store/news/getters.js
@@ -6,19 +6,5 @@ export default {
             window.location.reload();
         }
         return headline;
-    },
-    currentPhoto: state => {
-        const imgSrc = require(`@/assets/anniversary/${state.currentPhoto}.jpg`);
-        const img = new Image();
-        img.src = imgSrc;
-
-        const orientation = img.height > img.width ? 'portrait' : 'landscape';
-
-        const data = {
-            src: imgSrc,
-            orientation
-        }
-
-        return data;
     }
 }
